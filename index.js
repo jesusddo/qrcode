6a79d2838153111502d98eaccef292a82afb8dc9
DOUGLAS JESUS DA SILVA <JESUSDDO@basfad.basf.net>
2020-06-02 14:24:02 -0300

    tester

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const express = require('express')
-const routes=require('./routes')
+const routes=require('./src/routes')
 const cors=require('cors')
 const app=express()
 
-app.use(express.static(__dirname+'/build/'))
+app.use(express.static(__dirname+'/src/build/'))
 app.use(cors())
 app.use(express.json())
 app.use(routes)
