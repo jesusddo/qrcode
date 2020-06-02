f5c9ed6bd9505660a8be09cc3bf947aaaa4f8873
DOUGLAS JESUS DA SILVA <JESUSDDO@basfad.basf.net>
2020-06-02 16:56:57 -0300

    dsd

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const express = require('express')
 //const routes=require('./src/routes')
-const cors=require('cors')
+//const cors=require('cors')
 const app=express()
 
-app.use(express.static(__dirname))
-app.use(cors())
-app.use(express.json())
+app.use(express.static(__dirname))//+'/src/build/'))
+//app.use(cors())
+//app.use(express.json())
 //app.use(routes)
 
 const PORT=3000
